699bb4cadf10a156757c08eb90f4ef98c0676493
byronsanchez0 <byroncarrillo52@yahoo.es>
2023-06-01 15:09:30 -0600

    cleaning code and packaging-

--- app/src/main/java/com/example/movieappcompose/models/RetrofitClient.kt
+++ app/src/main/java/com/example/movieappcompose/models/RetrofitClient.kt
@@ -4,23 +4,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-//    private const val BASE_URL = "https://www.omdbapi.com/"
-//    const val API_KEY = "34a8034"
-//
-//    private val retrofit: Retrofit by lazy {
-//        Retrofit.Builder()
-//        .baseUrl(BASE_URL)
-//        .addConverterFactory(GsonConverterFactory.create())
-//        .build()
-//    }
-//
-//
-//    val movieService: MoviesService by lazy{
-//        retrofit.create(MoviesService::class.java)
-//    }
-private const val BASE_URL = "https://www.omdbapi.com/"
+    private const val BASE_URL = "https://www.omdbapi.com/"
 
-    private val retrofit : Retrofit by lazy {
+    private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
@@ -30,11 +16,11 @@ private const val BASE_URL = "https://www.omdbapi.com/"
     private val omdbApiService: MoviesService by lazy {
         retrofit.create(MoviesService::class.java)
     }
-//    java.lang.NullPointerException: Parameter specified as non-null is null: method androidx.compose.material3.TextKt.Text--4IGK_g, parameter text
+
     suspend fun searchMovies(query: String): List<Movie> {
         val apiKey = "34a8034"
         val response = omdbApiService.searchMovies(apiKey, query)
-        if (response.isSuccessful){
+        if (response.isSuccessful) {
             val searchResponse = response.body()
             return searchResponse?.search ?: emptyList()
         } else {
